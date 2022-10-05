e51a0c9b60a0e571f37f48649122e5cec2d7e779
Chuston13 <cdhprogrammer@gmail.com>
2022-10-05 13:59:55 -0500

    index.html

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+const button = document.querySelector('input[type="submit"]');
+const pullInfo = document.querySelector('input[type="text"]');
+const display = document.querySelector('.display')
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=953aea69fd04e5118320767efc5fc8b6')
+   
+   
+    button.addEventListener('click',function(){
+        fetch('http://api.openweathermap.org/data/2.5/weather?q='+pullInfo.value+'&APPID=953aea69fd04e5118320767efc5fc8b6')
 .then((response) => response.json())
-.then((data) => console.log(data));
+.then((data) => console.log(data)
+
+    )})
+   
+    
 
